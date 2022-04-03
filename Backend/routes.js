d615d13a51c5d59233e7a48962d641c834dde23c
vishalanap <vishalbanap@gmail.com>
2022-04-03 15:24:59 +0530

    backend routes for functions

--- Backend/routes.js
+++ Backend/routes.js
@@ -207,4 +207,142 @@ router.post('/approve', async(req,res) => {
 })
 
 
+
+// Declines the current trip request
+router.post('/decline', async(req,res) => {
+    const {trip_id} = req.body
+    connection.query(`DELETE FROM trip3 where trip_id="${trip_id}"; DELETE FROM books where trip_id="${trip_id}"`,[1,2],(e,op) => {
+        if(e){
+            console.log(e)
+        }
+        res.status(200).json({msg : 'Done'})
+    })
+})
+
+// Checks the details of the last trip that was booked by the user
+router.post('/checkstatus', async(req,res) => {
+    const {user_id, trip_id} = req.body
+    console.log(user_id, trip_id)
+    connection.query(`SELECT * FROM ongoing where user_id="${user_id}"`, (e,op) => {
+        if(e) {
+            console.log(e)
+            return res.status(400).json({msg: 'Error'})
+        }
+        else{
+            if(op.length !== 0){
+                return res.status(200).json({'msg' : 'approved'})
+            }
+            else{
+                connection.query(`SELECT * FROM trip4 where trip_id="${trip_id}" and status=0`, (err,opt) => {
+                    if(err){
+                        console.log(err)
+                    }
+                    else if(opt.length !== 0){
+                            console.log('in')
+                            return res.status(200).json({'msg' : 'wait'})
+                    }
+                    else{
+                        return res.status(200).json({'msg' : 'declined'})
+                    }
+                })
+            }
+        }
+    })
+})
+
+
+//  Adding a new driver
+router.post('/addnew', async(req,res) => {
+    const {driver_id, taxi_id, d_name, d_phone_no, rating, number, color, model, cclass, capacity} = req.body
+    console.log(number)
+    const sql = `insert into taxi1 values("${taxi_id}","${color}","${number}", NULL,"${model}");
+            insert into taxi2 values("${taxi_id}","${model}");
+            insert into taxi3 values("${model}", ${capacity}, "${cclass}");
+            insert into driver1 values("${driver_id}","${d_name}","${d_phone_no}","${taxi_id}",${rating});
+            insert into driver2 values("${driver_id}","${d_phone_no}");
+            insert into driver3 values("${d_phone_no}","${d_name}");
+            UPDATE taxi1 SET driver_id="${driver_id}" where taxi_id="${taxi_id}";
+            insert into works values("${driver_id}","2");
+            insert into drives values("${driver_id}","${taxi_id}");
+            insert into present_at values("${driver_id}","410078");
+            insert into availability values("${taxi_id}","410078");`
+    connection.query(sql,[1,2,3,4,5,6,7,8,9,10,11], (e,op) => {
+        if(e){
+            console.log(e)
+            res.status(404).json({'msg' : 'Error'})
+        }
+        else{
+            return res.status(200).json({'msg' : 'Inserted'})
+        }
+    })
+})
+
+// Retrieve information about the garage
+
+router.get('/getgarage', async(req,res) => {
+    connection.query('select * from taxi1 t inner join garage g on t.taxi_id = g.taxi_id;', (e,op) =>{
+        if(e){
+            console.log(e)
+            return res.status(404).json({'msg' : 'Error'})
+        }
+        else{
+            return res.status(200).json({garage : op})
+        }
+    })
+})
+
+// Get details about a trip
+router.post('/curtrip', async(req,res) => {
+    const {trip_id } = req.body
+    connection.query(`SELECT * FROM trip2 where trip_id="${trip_id}"`, (e,op) => {
+        if(e){
+            console.log(e)
+            return res.status(400).json({'msg' : 'Error'})
+        }
+        else{
+            return res.status(200).json({ongoing : op})
+        }
+    })
+})
+
+// Give rating to the driver
+router.post('/setrating', async(req,res) => {
+    const {rating , trip_id} = req.body
+    let prevrating = 0
+    connection.query(`SELECT rating from trip4 where trip_id="${trip_id}"`,(err,op) => {
+        if(err){
+            return res.status(404)
+        }
+        else{
+            prevrating = op[0].rating
+        }
+    })
+    console.log(prevrating)
+    const r = Math.ceil((rating + prevrating) / 2)
+    connection.query(`UPDATE trip4 set rating="${r}" where trip_id="${trip_id}";DELETE from ongoing where trip_id="${trip_id}"`,[1,2], (e,op) => {
+        if(e){
+            console.log(e)
+            return res.status(400).json({'msg' : 'Error'})
+        }
+        return res.status(200).json({'msg' : 'Updated'})
+    })
+
+})
+
+// Get details about the ongoing trip
+router.post('/getongoing', async(req,res) => {
+    const {user_id} = req.body
+
+    connection.query(`SELECT trip_id from ongoing where user_id="${user_id}";`, (e,op) => {
+        if(e){
+            console.log(e)
+        }
+        else{
+            return res.status(200).json({ongoing : op})
+        }
+    })
+})
+
+
+
 module.exports = router
