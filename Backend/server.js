3f7a58e8210437bd67112c68ce0487fa3ee684c4
Muddayya <muddayyaswami123@gmail.com>
2022-04-03 15:14:11 +0530

    databse query commands

--- Backend/server.js
+++ Backend/server.js
@@ -71,3 +71,52 @@ app.post('/logindriver', async (req,res) => {
         }
     })
 })
+
+
+//  Route for getting all the trip history of the user
+app.post('/gettrips', async(req,res) => {
+    const {user_id} = req.body
+    connection.query(`select t.from_s, t.to_d, fare from trip2 t inner join trip3 k on t.trip_id = k.trip_id where k.user_id ="${user_id}";`, (e,op) => {
+        if(e){
+            console.log(e)
+            return res.status(404).json({'msg': 'Some error occured' })
+        }
+        else{
+            if(op.length === 0) 
+                return res.status(200).json({data: op})
+            const from = op[0].from_s 
+            const to = op[0].to_d
+            connection.query(`SELECT * FROM trip2 WHERE from_s="${from}" and to_d="${to}"`, (e,opt) =>{
+                console.log(opt)
+            })
+            return res.status(200).json({'msg': 'Sucess',data : op})
+        }
+
+    }) 
+})
+
+//  Get the driver's taxi details
+app.post('/gettaxi',async (req,res) => {
+    const {driver_id} = req.body
+    connection.query(`SELECT * FROM taxi1 WHERE driver_id="${driver_id}"`, (e,op) => {
+        if(e){
+            return res.status(400).json({'msg' : 'Error occured'})
+        }
+        else{
+            res.status(200).json({msg : 'Success',data : op})
+        }
+    })
+})
+
+// Get drivers current location
+app.get('/getlocation',async (req,res) => {
+    connection.query(`SELECT * FROM location`, (e,op) => {
+        if(e){
+            return res.status(400).json({'msg' : 'Error occured'})
+        }
+        else{
+            res.status(200).json({msg : 'Success',data : op})
+        }
+    })
+})
+
