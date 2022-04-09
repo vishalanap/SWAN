53916f07880378dc74f67b8a0dd57896d43b0330
Muddayya <muddayyaswami123@gmail.com>
2022-04-09 17:39:49 +0530

    updated app.js for frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+mport React from 'react';
 import './App.css';
+import Landing from './pages/landingPage';
+import Customer from './pages/customerLogin';
+import Driver from './pages/driverLogin';
+import Admin from './pages/adminLogin';
+
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {DarkTheme, BaseProvider, styled} from 'baseui';
+import  { Route, BrowserRouter } from 'react-router-dom'
+const engine = new Styletron();
+const Centered = styled('div', {
+  height: '100%',
+  width : '100%'
+});
 
 function App() {
   return (
+    <StyletronProvider value={engine}>
+    <BaseProvider theme={DarkTheme}>
+      <Centered>
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path = "/customer" component = {Customer}/>
+      <Route exact path = "/driver" component = {Driver}/>
+      <Route exact path = "/admin" component = {Admin}/>
+      <Route exact path = "/" component = {Landing}/>
+
     </div>
+    </BrowserRouter>
+      </Centered>
+    </BaseProvider>
+  </StyletronProvider>
   );
 }
 
