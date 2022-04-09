e3dc60ca0fb3c7cd4d6085ccd3fd504887a70151
Muddayya <muddayyaswami123@gmail.com>
2022-04-09 17:32:56 +0530

    updated server file

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use('/api',router)
 // Route for customer login
 app.post('/login', async (req,res) => {
     const {name, pass }= req.body
-    connection.query(`SELECT * FROM user WHERE name="${name}" and user_id="${pass}";`, (e,op) => {
+    connection.query(`SELECT * FROM user1 WHERE name="${name}" and user_id="${pass}";`, (e,op) => {
         if(e){
             console.log(e)
             return res.status(404).json({'msg': 'Some error occured' })
@@ -40,7 +40,7 @@ app.post('/login', async (req,res) => {
 app.post('/register', async (req,res) => {
     console.log(req.body)
     const {name, pass ,add, ph}= req.body
-    const q = `INSERT INTO users values("${pass}","${name}","${ph}","${add}");`
+    const q = `INSERT INTO user1 values("${pass}","${name}");INSERT INTO user2 values("${pass}","${ph}");INSERT INTO user3 values("${pass}","${add}");INSERT INTO user4 values("${ph}","${name}");`
     connection.query(`${q}`,[1,2,3,4] ,(e,op) => {
         if(e){
             console.log(e)
@@ -55,7 +55,7 @@ app.post('/register', async (req,res) => {
 //  Route for driver login
 app.post('/logindriver', async (req,res) => {
     const {name, pass }= req.body
-    connection.query(`SELECT * FROM driver WHERE name="${name}" and driver_id="${pass}";`, (e,op) => {
+    connection.query(`SELECT * FROM driver1 WHERE d_name="${name}" and driver_id="${pass}";`, (e,op) => {
         if(e){
             console.log(e)
             return res.status(404).json({'msg': 'Some error occured' })
@@ -73,50 +73,22 @@ app.post('/logindriver', async (req,res) => {
 })
 
 
-//  Route for getting all the trip history of the user
-app.post('/gettrips', async(req,res) => {
-    const {user_id} = req.body
-    connection.query(`select start_place, destination, fare from trip where user_id ="${user_id}";`, (e,op) => {
+//  Route for admin login
+app.post('/loginadmin', async (req,res) => {
+    const {name, pass }= req.body
+    connection.query(`SELECT * FROM admin WHERE admin_name ="${name}" and admin_id="${pass}";`, (e,op) => {
         if(e){
             console.log(e)
             return res.status(404).json({'msg': 'Some error occured' })
         }
         else{
-            if(op.length === 0) 
-                return res.status(200).json({data: op})
-            const from = op[0].start
-            const to = op[0].to_d
-            connection.query(`SELECT * FROM trip2 WHERE from_s="${from}" and to_d="${to}"`, (e,opt) =>{
-                console.log(opt)
-            })
-            return res.status(200).json({'msg': 'Sucess',data : op})
-        }
-
-    }) 
-})
-
-//  Get the driver's taxi details
-app.post('/gettaxi',async (req,res) => {
-    const {driver_id} = req.body
-    connection.query(`SELECT * FROM taxi where taxi_id = ( SELECT taxi_id FROM driver WHERE driver_id="${driver_id}");`, (e,op) => {
-        if(e){
-            return res.status(400).json({'msg' : 'Error occured'})
-        }
-        else{
-            res.status(200).json({msg : 'Success',data : op})
+            if(op.length === 0){
+                console.log('yes')
+                return res.status(200).json({'msg' : 'Invalid creds'})
+            }
+            else{
+                return res.status(200).json({'msg': 'Success', data : op})
+            }
         }
     })
 })
-
-// Get drivers current location
-app.get('/getlocation',async (req,res) => {
-    connection.query(`SELECT * FROM location`, (e,op) => {
-        if(e){
-            return res.status(400).json({'msg' : 'Error occured'})
-        }
-        else{
-            res.status(200).json({msg : 'Success',data : op})
-        }
-    })
-})
-
